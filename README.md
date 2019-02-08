e5b6eb2b567c4a9c9b23379dfb47a4390f603944
Chet Corcos <ccorcos@gmail.com>
2019-02-07 19:58:40 -0800

    First

--- README.md
+++ README.md
@@ -1,12 +1,83 @@
-# TypeScript Library Boilerplate
+# Stock Cutting Problem Solver
+
+This program will help you figure out how much wood you need to buy from the store and how to cut that wood to get all the quantities of all the sizes of wood that you need.
+
+## How it works
+
+I'm going to steal some numbers from pa real project of mine](https://github.com/ccorcos/couch).
+
+Suppose you need the following pieces of wood (size in inches).
+
+```ts
+const requiredPieces = [
+	{ size: 11, count: 28 },
+	{ size: 21, count: 14 },
+	{ size: 84, count: 8 },
+	{ size: 3.5, count: 42 },
+	{ size: 79.5, count: 4 },
+]
+```
+
+You go to the store and find out that they sell boards that are 96 inches long. You can find out how many boards you need and how to cut them by running this through optimization progam.
+
+```ts
+const stockSize = 96
+const result = howToCutBoards1D(stockSize, requiredPieces)
+```
+
+The result will look something like this:
+
+```ts
+[
+	{
+		count: 5,
+		cuts: [11, 11, 11, 21, 21, 21] },
+	{
+		count: 1,
+		cuts: [ 11, 11, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5],
+	},
+	{
+		count: 8,
+		cuts: [11, 84]
+	},
+	{
+		count: 4,
+		cuts: [11, 3.5, 79.5]
+	},
+	{
+		count: 1,
+		cuts: [ 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5],
+	},
+]
+```
+
+Now we know that we will need 19 boards and how to cut all each of them!
 
 ## Getting Started
 
 ```sh
-git clone git@github.com:ccorcos/typescript-library.git library
-cd library
-git remote remove origin
+npm install --save stock-cutting
+```
+
+```ts
+import { howToCutBoards1D } from "stock-cutting"
+```
+
+## Other Notes
+
+The [Stock Cutting Problem](https://en.wikipedia.org/wiki/Cutting_stock_problem) is an [NP-Hard](https://en.wikipedia.org/wiki/NP-hardness) problem. This means that there's no known "fast" solution to this problem. This program will work reasonably fast for most real use cases, but at the worst case, it has an exponential runtime.
+
+## Development
+
+```sh
 npm run build
 npm version major
 npm publish
 ```
+
+### To Do
+
+- [x] 1d solver
+- [ ] saw blade size
+- [ ] multiple stock sizes optimization
+- [ ] 2d
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,126 @@
-export function add(x: number, y: number): number {
-  return x + y
+import * as _ from "lodash"
+import * as solver from "javascript-lp-solver/src/solver"
+
+function isSubset(a: Array<number>, b: Array<number>) {
+	const a2 = [...a]
+	for (const n of b) {
+		const i = a2.indexOf(n)
+		if (i !== -1) {
+			a2.splice(i, 1)
+			if (a2.length === 0) {
+				return true
+			}
+		}
+	}
+	return a2.length === 0
+}
+
+/**
+ * Given a board of `size` and a list of `cuts` you
+ * can make out of the board, how many unique ways of cutting the board
+ * are there?
+ */
+export function howManyWays(
+	size: number,
+	cuts: Array<number>,
+	state: Array<number> = []
+): Array<Array<number>> {
+	return _.uniqWith(
+		_.flatten(
+			cuts.map(cut => {
+				const remainder = size - cut
+				if (remainder >= 0) {
+					return howManyWays(remainder, cuts, [...state, cut])
+				} else {
+					return [state]
+				}
+			})
+		),
+		(x, y) => isSubset(x, y) || isSubset(y, x)
+	)
+}
+
+type RequiredCuts = Array<{ size: number; count: number }>
+type ResultCuts = Array<{ count: number; decimal: number; cuts: Array<number> }>
+
+/**
+ * Given a stock side of wood you and buy, how many do I need and how do I cut it
+ * in order to make enough pieces of with at the given sizes.
+ */
+export function howToCutBoards1D(
+	stockSize: number,
+	requiredCuts: RequiredCuts
+): ResultCuts {
+	const cutSizes = requiredCuts.map(({ size }) => size)
+
+	const waysOfCutting = howManyWays(stockSize, cutSizes)
+
+	// Transform [1,1,2,3] into {cut1: 2, cut2: 1, cut3: 3}.
+	// Each will be the different versions of cutting the stock board.
+	const stockVersions = waysOfCutting.map(way => {
+		const stockCut = {}
+		for (const cut of cutSizes) {
+			stockCut["cut" + cut] = 0
+		}
+		for (const cut of way) {
+			stockCut["cut" + cut] = stockCut["cut" + cut] + 1
+		}
+		// stockCut["remainder"] = stockSize - _.sum(way)
+		return stockCut
+	})
+
+	// Create a variable for each version with a count: 1 which we will minimize.
+	const variables = stockVersions
+		.map((cut, index) => ({ ["version" + index]: { ...cut, count: 1 } }))
+		.reduce((acc, next) => ({ ...acc, ...next }))
+
+	// We can't puchase part of a board, so the result but me an int, not a float.
+	const ints = stockVersions
+		.map((cut, index) => ({ ["version" + index]: 1 }))
+		.reduce((acc, next) => ({ ...acc, ...next }))
+
+	// Create constraints from the required cuts with a min on the count required.
+	const constraints = requiredCuts
+		.map(({ size, count }) => ({ ["cut" + size]: { min: count } }))
+		.reduce((acc, next) => ({ ...acc, ...next }))
+
+	// Create out model for the simplex linear programming solver.
+	// https://github.com/JWally/jsLPSolver
+	const model = {
+		optimize: "count",
+		opType: "min",
+		variables: variables,
+		int: ints,
+		constraints: constraints,
+	}
+
+	// Run the program
+	const results = solver.Solve(model)
+
+	// console.log(model)
+	// console.log(results)
+
+	if (!results.feasible) {
+		throw new Error("Didn't work")
+	}
+
+	const resultCuts: ResultCuts = []
+
+	for (let i = 0; i < waysOfCutting.length; i++) {
+		const number = results["version" + i]
+		if (number !== undefined && number > 0) {
+			// Need to take the ceiling because even though we're using integer mode,
+			// the final cuts will still have a remainder balance which computes to
+			// the remainder decimal. We'll store the raw decimal in there in case you
+			// want to use it somewhere else.
+			// https://github.com/JWally/jsLPSolver/issues/84
+			resultCuts.push({
+				count: Math.ceil(number),
+				decimal: number,
+				cuts: waysOfCutting[i],
+			})
+		}
+	}
+
+	return resultCuts
 }
